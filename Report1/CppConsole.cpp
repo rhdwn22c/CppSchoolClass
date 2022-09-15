2eca76fa80c965f7eadfc33b461245d935a11914
rhdwn22c <rhdwn22c@gmail.com>
2022-09-15 20:13:40 +0900

    performance third(?)

--- Report1/CppConsole.cpp
+++ Report1/CppConsole.cpp
@@ -1,46 +1,57 @@
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
 class Bank {
 public:
-    int initialMoney;
-    int savingMonth;
+    int money;
+    int year;
 };
 
 class BankUser {
 public:
-    int cal(Bank bank);
+    int cal(Bank bank, float yearInt);
     int calTax(int monRes);
 
-    float taxPer = 15.4f;
-    float yearInt = 3.55f;
+    float taxPer = 0.154f;
+    float sYearInt = 4.0f;
+    float nYearInt = 3.0f;
 };
 
-int BankUser :: cal(Bank bank) {
+int BankUser::cal(Bank bank, float yearInt) {
     int monRes;
-    monRes = bank.initialMoney * yearInt / 100 * (bank.savingMonth + 1) * bank.savingMonth / 2 / 12;
+    monRes = bank.money * yearInt / 100 * (bank.year + 1) * bank.year / 2 / 12;
     return monRes;
 }
 
-int BankUser :: calTax(int monRes) {
+int BankUser::calTax(int monRes) {
     int taxRes;
-    taxRes = monRes - monRes * taxPer / 100;
+    taxRes = monRes - monRes * taxPer;
     return taxRes;
 }
 
 int main() {
-    int monRes, taxRes;
+    int monRes = 0, taxRes = 0;
+    int whatSaving;
 
     BankUser user;
     Bank bank;
 
-    cin >> bank.initialMoney;
-    cin >> bank.savingMonth;
+    cout << "어떤 적금을 가입하시겠습니까? (특판 : 1, 일반 : 2)";
+    cin >> whatSaving;
+    cout << "얼마 입금하시겠습니까? : ";
+    cin >> bank.money;
+    cout << "몇 개월 입금하시겠습니까? : ";
+    cin >> bank.year;
 
-    monRes = user.cal(bank);
+    if (whatSaving == '1') {
+        monRes = user.cal(bank, user.sYearInt);
+    }
+    else if (whatSaving == '2') {
+        monRes = user.cal(bank, user.nYearInt);
+    }
     taxRes = user.calTax(monRes);
-    monRes += bank.initialMoney * bank.savingMonth;
-    taxRes += bank.initialMoney * bank.savingMonth;
+    monRes += bank.money * bank.year;
+    taxRes += bank.money * bank.year;
 
     cout << "세전 : " << monRes << endl;
     cout << "세후 : " << taxRes;
--- Report3/sdBank.cpp
+++ Report3/sdBank.cpp
@@ -1,54 +1,65 @@
 #include <iostream>
 using namespace std;
 #define tax 0.154f;
-
-class Bank {
+class User
+{
 public:
+    string name = "";
+    int id = 0;
+};
+
+class Bank
+{
+public:
+    User* user = new User[50];
+    
     int cal(Bank bank, float yearInterest);
     int calTax(int moneyResult);
 
     int initialMoney;
     int savingMonth;
-};
 
-class BankUser {
-public:
     float savingInterest = 4.0f;
     float depositInterest = 3.0f;
 };
 
-int Bank::cal(Bank bank, float yearInterest) {
+int Bank::cal(Bank bank, float yearInterest)
+{
     int noneTaxMoneyResult;
     noneTaxMoneyResult = bank.initialMoney * yearInterest * (bank.savingMonth + 1) * bank.savingMonth / 2 / 12;
     return noneTaxMoneyResult;
 }
 
-int Bank::calTax(int moneyResult) {
+int Bank::calTax(int moneyResult)
+{
     int taxMoneyResult;
     taxMoneyResult = moneyResult - moneyResult * tax;
     return taxMoneyResult;
 }
 
-int main() {
+int main()
+{
     int noneTaxMoneyResult, taxMoneyResult;
     int whatSaving;
 
-    BankUser bankUser;
     Bank bank;
 
-    cout << "어떤 적금을 가입하시겠습니까? (특판 : 1, 일반 : 2)";
+    cout << "적금 하시겠습니까, 예금 하시겠습니까? (적금 : 1 / 예금 : 2)";
     cin >> whatSaving;
     cout << "얼마 입금하시겠습니까? : ";
     cin >> bank.initialMoney;
     cout << "몇 개월 입금하시겠습니까? : ";
     cin >> bank.savingMonth;
 
-    if (whatSaving == '1') {
-        noneTaxMoneyResult = bank.cal(bank, bankUser.savingInterest);
+    if (whatSaving == '1')
+    {
+        noneTaxMoneyResult = bank.cal(bank, bank.savingInterest);
     }
-    else if (whatSaving == '2') {
-        noneTaxMoneyResult = bank.cal(bank, bankUser.depositInterest);
+    else if (whatSaving == '2')
+    {
+        noneTaxMoneyResult = bank.cal(bank, bank.depositInterest);
     }
+
     taxMoneyResult = bank.calTax(noneTaxMoneyResult);
     noneTaxMoneyResult += bank.initialMoney * bank.savingMonth;
     taxMoneyResult += bank.initialMoney * bank.savingMonth;
