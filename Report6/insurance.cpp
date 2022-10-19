0fe347de44dc100864faf585df6003f30db25cb6
rhdwn22c <rhdwn22c@gmail.com>
2022-10-19 15:58:05 +0900

    Update

--- Report6/insurance.cpp
+++ Report6/insurance.cpp
@@ -1,16 +1,10 @@
 #include <iostream>
 using namespace std;
 #define YEAR 12
-#define TAXFREE 100000
+#define TAXFREE 200000
 
 class Insurance {
 public:
-	float nationalPension = 0.045;
-	float healthInsurance = 0.03495;
-	float longTermCareInsurance = 0.1227;
-	float employmentInsurance = 0.009;
-	float localIncomeTax = 0.1;
-
 	int NationalPensionCalculate(int money) {
 		return money * nationalPension;
 	}
@@ -30,6 +24,17 @@ public:
 	int LocalIncomeTaxCalculate(int incomeTax) {
 		return incomeTax * localIncomeTax;
 	}
+
+	int FinalInsuranceCalculate(int incomeTax, int money) {
+		return NationalPensionCalculate(money) - HealthInsuranceCalculate(money) - LongTermCareInsuranceCalculate(money) - EmploymentInsuranceCalculate(money) - incomeTax - LocalIncomeTaxCalculate(incomeTax);
+	}
+
+private:
+	float nationalPension = 0.045;
+	float healthInsurance = 0.03495;
+	float longTermCareInsurance = 0.1227;
+	float employmentInsurance = 0.009;
+	float localIncomeTax = 0.1;
 };
 
 
@@ -43,19 +48,11 @@ int main() {
 	cout << "소득세가 얼마인가요? : ";
 	cin >> incomeTax;
 
-	money = (money / YEAR) - (TAXFREE * 2);
+	money = (money / YEAR) - TAXFREE;
 
-	cout << money << endl;
-	cout << insurance.NationalPensionCalculate(money) << endl;
-	cout << insurance.HealthInsuranceCalculate(money) << endl;
-	cout << insurance.LongTermCareInsuranceCalculate(money) << endl;
-	cout << insurance.EmploymentInsuranceCalculate(money) << endl;
-	cout << incomeTax << endl;
-	cout << insurance.LocalIncomeTaxCalculate(incomeTax) << endl;
-
-	money = money - insurance.NationalPensionCalculate(money) - insurance.HealthInsuranceCalculate(money) - insurance.LongTermCareInsuranceCalculate(money) - insurance.EmploymentInsuranceCalculate(money) - incomeTax - insurance.LocalIncomeTaxCalculate(incomeTax);
+	money = money - insurance.FinalInsuranceCalculate(money, incomeTax);
 	
-	cout << "월 수령액 : " << money + (TAXFREE * 2) << endl;
+	cout << "월 수령액 : " << money + TAXFREE << endl;
 
 	return 0;
 }
